8dc31ab29ecf9d63190fa823bc268ed1c6615960
Mike Zeng <mzen@starlightx.io>
2023-08-07 13:06:06 -0500

    added support for Promotheus monitoring

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 import os
+import time
 
 from dotenv import load_dotenv
 load_dotenv()  
@@ -39,36 +40,69 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from prometheus_client import make_asgi_app
+from prometheus_client import multiprocess
+from prometheus_client import generate_latest, CollectorRegistry, CONTENT_TYPE_LATEST, Counter
+from prometheus_client import Counter, Info, Gauge
+
+def make_metrics_app():
+    registry = CollectorRegistry()
+    multiprocess.MultiProcessCollector(registry)
+    return make_asgi_app(registry=registry)
+
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
+
+# Metrics for promotheus
+req = Counter('total_requests', 'Total requests to the server.')
+embed = Counter('total_embedds', 'Total requests to the embedding server.')
+emot = Counter('total_emotions', 'Total requests to the emotion server.')
+invalid = Counter('invalid_auth_count', 'Total number of authentication failures.')
+llamaReq = Counter('total_llama', 'Total requests to the llama server.')
+
+embed_response_time = Gauge('embed_response_time', 'Average Time for an embedding request to finish')
+emotions_response_time = Gauge('emotions_response_time', 'Average Time for an embedding request to finish')
+llama_response_time = Gauge('llama_response_time', 'Average Time for an embedding request to finish')
+
+
+i = Info('sxis_system_data', 'Env variables for SXIS')
+i.info({'version': '1.0.0', 'type':type})
+
+
 @app.get("/")
 async def root():
+    req.inc()
     return {"message": "Working Fine, hash: 0x1234567890"}
 
 
 @app.post("/encode")
 async def encode(input: BasicReq):
-    print(secret)
+    start = time.time()
     if (secret is not None and input.key != secret):
+        invalid.inc()
         raise HTTPException(status_code=401, detail="Invalid Key")
+    embed.inc()
+    embed_response_time.set(time.time() - start)
     return {"embedding":encoder.getEmbeddings(input.message)}
 
 
 @app.post("/emotion")
 async def emotiongen(input: BasicReq):
-    print(secret)
+    start = time.time()
     if (secret is not None and input.key != secret):
+        invalid.inc()
         raise HTTPException(status_code=401, detail="Invalid Key")
+    emot.inc()
+    emotions_response_time.set(time.time() - start)
     return {"emotion": emotion.get_emotion(input.message)}
-
-@app.get("/test-emotion")
-async def test_emotion_gen():
-    return {"emotion": emotion.get_emotion("Hi!!!")}
-
 
 
 @app.post("/completion")
 async def llamaRequest(input: LlamaReq):
-    print(secret)
+    start = time.time()
     if (secret is not None and input.key != secret):
+        invalid.inc()
         raise HTTPException(status_code=401, detail="Invalid Key")
+    llama_response_time.set(time.time() - start)
     return {"response":llama.sendReq(input.message, input.max, input.temp)}
 
